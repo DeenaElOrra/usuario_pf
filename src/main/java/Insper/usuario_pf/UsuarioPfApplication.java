289b205442cdfc8c92197a6e6761217bbe57ce94
Deena <124579700+DeenaElOrra@users.noreply.github.com>
2024-11-11 08:24:30 -0300

    Add files via upload

---
+++ src/main/java/Insper/usuario_pf/UsuarioPfApplication.java
@@ -0,0 +1,13 @@
+package Insper.usuario_pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioPfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioPfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Insper/usuario_pf/UsuarioPfApplicationTests.java
@@ -0,0 +1,13 @@
+package Insper.usuario_pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioPfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
